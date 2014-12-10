eb57c1d29fbe22deda8de45096a27d4d0942b2c5
Dave <davy.bolivar@gmail.com>
2014-12-11 00:49:30 +0800

    Fixed Ball bounce speed

--- Assets/CurveX.cs
+++ Assets/CurveX.cs
@@ -5,7 +5,7 @@ public class CurveX : MonoBehaviour {
 	public float dur;
 	public float min, max;
 	public GameObject ballCollider;
-
+	public float offset;
 	// Use this for initialization
 	void Start () {
 		//min = ballCollider.transform.localPosition.y;
@@ -14,10 +14,11 @@ public class CurveX : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		dur = ballCollider.transform.rigidbody2D.velocity.x/3f;
-		min = ballCollider.transform.localPosition.y;
+		dur = ballCollider.transform.rigidbody2D.velocity.x/4f;
+		min = ballCollider.transform.localPosition.y+offset;
 		max = ballCollider.transform.localPosition.y + 1f;
 		transform.position = new Vector3(ballCollider.transform.localPosition.x,  Mathf.PingPong(Time.time * dur, max-min)+min, transform.localPosition.z);
-		//transform.localPosition = new Vector3(transform.localPosition.x, Mathf.PingPong(Time.time * bodyDur, bodyMax-bodyMin)+bodyMin, body.transform.localPosition.z);
+		//transform.position = Vector3.Lerp(ballCollider.transform.localPosition,new Vector3(ballCollider.transform.localPosition.x,  Mathf.PingPong(Time.time * dur, max-min)+min, transform.localPosition.z),
+		//                                  .5f);
 	}
 }
