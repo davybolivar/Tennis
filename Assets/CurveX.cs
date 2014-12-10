ed8c80b82ae73a61baed33e3a2e4ab6001321358
Dave <davy.bolivar@gmail.com>
2014-12-11 00:32:48 +0800

    Added ball bounce animation

--- Assets/CurveX.cs
+++ Assets/CurveX.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class CurveX : MonoBehaviour {
+	public float dur;
+	public float min, max;
+	public GameObject ballCollider;
 
 	// Use this for initialization
 	void Start () {
-	
+		//min = ballCollider.transform.localPosition.y;
+		//max = ballCollider.transform.localPosition.y + 1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		dur = ballCollider.transform.rigidbody2D.velocity.x/3f;
+		min = ballCollider.transform.localPosition.y;
+		max = ballCollider.transform.localPosition.y + 1f;
+		transform.position = new Vector3(ballCollider.transform.localPosition.x,  Mathf.PingPong(Time.time * dur, max-min)+min, transform.localPosition.z);
+		//transform.localPosition = new Vector3(transform.localPosition.x, Mathf.PingPong(Time.time * bodyDur, bodyMax-bodyMin)+bodyMin, body.transform.localPosition.z);
 	}
 }
