543b4bbf1b770893bafba3c66e9c9f959746667e
dave <davy.bolivar@gmail.com>
2014-12-15 10:37:05 +0800

    Added Main Menu

---
+++ Assets/LerpFrom.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpFrom : MonoBehaviour {
+	public float toX,dur;
+
+	// Update is called once per frame
+	void Update () {
+		LerpFromPos ();
+	}
+
+	void LerpFromPos(){
+		transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(toX,transform.localPosition.y,transform.localPosition.z),
+		                                       dur);
+	}
+}
---
+++ Assets/SceneManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+		
+	public void Play(){
+		Application.LoadLevel ("Play");
+	}
+
+	public void MainMenu()
+	{
+		Application.LoadLevel ("Menu");
+	}
+
+}
